fbfaa0857c148ef79677e8523ccbb36b2f734c2d
Cody Boggs <cody@freshtracks.io>
2018-09-06 13:39:37 -0600

    start cleaning up some silly things I did last time

--- run-local/run.sh
+++ run-local/run.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_DIR="${SCRIPT_DIR}/../.."
+PROJECT_DIR="${SCRIPT_DIR}/.."
 
 source ${SCRIPT_DIR}/func.sh
 
 function getCortexLiteImage() {
-  echo $(${DOCKER} images --filter=reference='quay.io/weaveworks/cortex-lite*:latest' --format="{{ .Repository }}:{{ .Tag }}" | sort | uniq)
+  echo $(${DOCKER} images --filter=reference='quay.io/weaveworks/${KS_ENV}*:latest' --format="{{ .Repository }}:{{ .Tag }}" | sort | uniq)
 }
 
 function buildLite() {
-  pushd ${PROJECT_DIR} > /dev/null
-  eval "$(${MK} docker-env -p cortex-lite)"
+  pushd ${PROJECT_DIR}
+  eval "$(${MK} docker-env -p ${KS_ENV})"
   make cmd/lite/.uptodate
   popd > /dev/null
 }
 
-echo "Checking for cortex-lite minikube profile..."
-${MK} status -p cortex-lite
+echo "Checking for ${KS_ENV} minikube profile..."
+${MK} status -p ${KS_ENV} > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "cortex-lite profile not found, creating ..."
-  ${MK} start --cpus 4 --memory 8192 --kubernetes-version v1.9.4 --profile cortex-lite --log_dir ${SCRIPT_DIR}/logs
+  echo "${KS_ENV} profile not found, creating ..."
+  ${MK} start --cpus 4 --memory 8192 --kubernetes-version v1.9.4 --profile ${KS_ENV} --log_dir ${SCRIPT_DIR}/logs
 fi
 
-eval "$(${MK} docker-env -p cortex-lite)"
-echo "cortex-lite minikube profile setup, continuing..."
+eval "$(${MK} docker-env -p ${KS_ENV})"
+echo "${KS_ENV} minikube profile setup, continuing..."
 
 CORTEX_LITE_IMG=$(getCortexLiteImage)
 
-echo "Checking for cortex-lite image..."
+echo "Checking for ${KS_ENV} image..."
 
 if [ -z "${CORTEX_LITE_IMG}" ]; then
-  echo "Need to build cortex-lite image..."
+  echo "Need to build ${KS_ENV} image..."
   buildLite
 
   if [ -z "${CORTEX_LITE_IMG}" ]; then
@@ -40,12 +40,11 @@ if [ -z "${CORTEX_LITE_IMG}" ]; then
   fi
 fi
 
-echo "Found cortex-lite image in minikube docker: ${CORTEX_LITE_IMG}. Continuing..."
+echo "Found ${KS_ENV} image in minikube docker: ${CORTEX_LITE_IMG}. Continuing..."
 
 echo "Applying ksonnet bits to ${KS_ENV} environment..."
 pushd ksonnet > /dev/null
-kubectl config use-context cortex-lite
-sed -ie 's/cortex_default/'${KS_ENV}'/g' app.yaml
+kubectl config use-context ${KS_ENV}
 ${KS} apply --insecure-skip-tls-verify ${KS_ENV}
 git checkout app.yaml
 popd > /dev/null
